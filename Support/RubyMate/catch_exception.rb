5b9ccbb365884e3e6cf58f0ae31ce61d01afe8eb
Ezekiel Templin <zeke@templ.in>
2012-02-09 13:09:09 -0500

    Drop CGI to avoid 1.9.3 segfault
    Fixes issue wherein running code via TextMate causes Ruby to segfault. Documented [here](https://gist.github.com/1333721/196a288f84355b9d8b7372d6834d25f5036f7ea5) including a [link](https://gist.github.com/1354592) to @vhyza's solution.
    
    It worked for me. Maybe it will work for others.

--- Support/RubyMate/catch_exception.rb
+++ Support/RubyMate/catch_exception.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 STDOUT.sync = true
 STDERR.sync = true
 
@@ -6,11 +8,11 @@ require 'pathname'
 at_exit do
   if (e = $!) && !e.instance_of?(SystemExit)
     require "#{ENV['TM_SUPPORT_PATH']}/lib/escape"
-    require "cgi"
+
     io = IO.for_fd(ENV['TM_ERROR_FD'].to_i)
 
     io.write "<div id='exception_report' class='framed'>\n"
-    io.write "<p id='exception'><strong>#{e.class.name}:</strong> #{CGI.escapeHTML e.message.sub(/`(\w+)'/, "\1")}</p>\n"
+    io.write "<p id='exception'><strong>#{e.class.name}:</strong> #{e.message.sub(/`(\w+)'/, '‘\1’').sub(/ -- /, ' — ')}</p>\n"
 
     io.write "<blockquote><table border='0' cellspacing='4' cellpadding='0'>\n"
 
@@ -24,13 +26,13 @@ at_exit do
           url = '&url=file://' + e_url(file)
           display_name = File.basename(file)
         end
-          
+
         io << "<tr><td><a class='near' href='txmt://open?line=#{line + url}'>"
-        io << (method ? "method #{CGI::escapeHTML method}" : '<em>at top level</em>')
-        io << "</a></td>\n<td>in <strong>#{CGI::escapeHTML display_name}</strong> at line #{line}</td></tr>\n"
+        io << (method ? "method #{method}" : '<em>at top level</em>')
+        io << "</a></td>\n<td>in <strong>#{display_name}</strong> at line #{line}</td></tr>\n"
       end
     end
-    
+
     io.write "</table></blockquote></div>"
     io.flush
 
